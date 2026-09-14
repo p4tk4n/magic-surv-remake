2b6b2642b2b82ab3634fdfce9739e179b0096ece
Michael <imtheearthbender97@gmail.com>
2026-09-14 20:59:01 +0200

    enemy tweaks

--- node components/healthbar/health_bar.gd
+++ node components/healthbar/health_bar.gd
@@ -9,7 +9,7 @@ var health_bar_style
 func _ready() -> void:
 	health_bar_style = get_theme_stylebox("fill").duplicate()
 	add_theme_stylebox_override("fill", health_bar_style)
-	health_bar_style.bg_color = health_to_color(player.current_health, player.max_health)
+	set_bg_color()
 	
 func set_bg_color():
 	health_bar_style.bg_color = health_to_color(player.current_health, player.max_health)
@@ -17,7 +17,6 @@ func set_bg_color():
 func update_health_bar():
 	value = player.current_health
 	health_bar_style.bg_color = health_to_color(player.current_health, player.max_health)
-	
 
 func health_to_color(current: float, max: float) -> Color:
 	var pct: float = clamp(current/max, 0.0, 1.0)
